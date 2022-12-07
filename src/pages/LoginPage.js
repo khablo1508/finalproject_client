a3f732f61cf94b3ae49ed481ee136404c5269f3b
khablo1508 <khablo.anna@gmail.com>
2022-12-07 15:46:14 +0100

    add sign pages,homepage,navbar

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import axios from 'axios';
 
 const API_URL = 'http://localhost:5005';
 
